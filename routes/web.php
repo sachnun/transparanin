1570154fdf24834dc6e28f45f11e7ab30cfd07ca
sachnun <sachnun12@gmail.com>
2022-07-11 22:18:34 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardAkunController;
+use App\Http\Controllers\DashboardAuthController;
+use App\Http\Controllers\DashboardBarangController;
+use App\Http\Controllers\DashboardActivityController;
+use App\Http\Controllers\DashboardPenerimaController;
+use App\Http\Controllers\DashboardTrackController;
+use App\Http\Controllers\DashboardValidasiController;
+use App\Http\Controllers\LandingPageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +22,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [LandingPageController::class, 'index']);
+
+// Route dashboard namespace
+Route::prefix('dashboard')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('ringkasan');
+
+    Route::get('/login', [DashboardAuthController::class, 'login'])->name('login');
+
+    Route::prefix('bantuan')->group(function () {
+        Route::resource('/barang', DashboardBarangController::class);
+        Route::resource('/penerima', DashboardPenerimaController::class);
+    });
+
+    Route::resource('/track', DashboardTrackController::class);
+
+    Route::prefix('validasi')->group(function () {
+        Route::get('/permintaan', [DashboardValidasiController::class, 'permintaan'])->name('validasi.permintaan');
+        Route::get('/terkirim', [DashboardValidasiController::class, 'terkirim'])->name('validasi.terkirim');
+        Route::get('/batal', [DashboardValidasiController::class, 'batal'])->name('validasi.batal');
+    });
+    Route::resource('/validasi', DashboardValidasiController::class)
+        ->only('store', 'update', 'destroy');
+
+    Route::resource('/akun', DashboardAkunController::class);
+    Route::get('/activity', [DashboardActivityController::class, 'index'])->name('activity');
 });
