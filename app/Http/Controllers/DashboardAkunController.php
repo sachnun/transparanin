a52328a7edb7d817ae7301a9564598f54389e4dc
sachnun <sachnun12@gmail.com>
2022-07-12 00:59:57 +0700

    update akun dashboard controller

--- app/Http/Controllers/DashboardAkunController.php
+++ app/Http/Controllers/DashboardAkunController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardAkunController extends Controller
 {
 
     public function index()
     {
-        return view('dashboard.akun.index');
+        $akuns = User::paginate(5);
+
+        return view('dashboard.akun.index', compact('akuns'));
     }
 
     public function create()
@@ -19,26 +24,128 @@ class DashboardAkunController extends Controller
 
     public function store(Request $request)
     {
-        //
+        // validasi
+        $validated = $request->validate([
+            'username' => 'required|unique:users',
+            'password' => 'required',
+            'nama_depan' => 'required',
+            'nama_belakang' => '',
+            'nik' => 'required|unique:users',
+            'alamat' => 'required',
+            'rt' => 'required',
+            'rw' => 'required',
+            'kelurahan' => 'required',
+            'kecamatan' => 'required',
+            'is_admin' => 'required',
+        ]);
+
+        // check avatar
+        if ($request->hasFile('avatar')) {
+            $avatar = $request->file('avatar');
+            $filename = Str::random(6) . '.' . $avatar->getClientOriginalExtension();
+
+            // upload ke folder
+            Storage::disk('public')->put($filename, file_get_contents($avatar));
+            $validated['avatar'] = $filename;
+        } else {
+            $validated['avatar'] = 'avatar.png';
+        }
+
+        // simpan data ke database
+        $akun = User::create([
+            'username' => $validated['username'],
+            'password' => bcrypt($validated['password']),
+            'avatar' => $validated['avatar'],
+            'nama_depan' => $validated['nama_depan'],
+            'nama_belakang' => $validated['nama_belakang'],
+            'nik' => $validated['nik'],
+            'alamat' => $validated['alamat'],
+            'rt' => $validated['rt'],
+            'rw' => $validated['rw'],
+            'kelurahan' => $validated['kelurahan'],
+            'kecamatan' => $validated['kecamatan'],
+            'is_admin' => $validated['is_admin'],
+        ]);
+
+        // redirect ke halaman show
+        return redirect()->route('akun.show', $akun->id);
     }
 
     public function show($id)
     {
-        return view('dashboard.akun.show');
+        $akun = User::findOrFail($id);
+
+        return view('dashboard.akun.show', compact('akun'));
     }
 
     public function edit($id)
     {
-        return view('dashboard.akun.edit');
+        $akun = User::findOrFail($id);
+
+        return view('dashboard.akun.edit', compact('akun'));
     }
 
     public function update(Request $request, $id)
     {
-        //
+        // validasi
+        $validated = $request->validate([
+            'username' => 'required|unique:users,username,' . $id,
+            'nama_depan' => 'required',
+            'nama_belakang' => '',
+            'nik' => 'required|unique:users,nik,' . $id,
+            'alamat' => 'required',
+            'rt' => 'required',
+            'rw' => 'required',
+            'kelurahan' => 'required',
+            'kecamatan' => 'required',
+            'is_admin' => 'required',
+        ]);
+
+        $akun = User::findOrFail($id);
+
+        // check avatar
+        if ($request->hasFile('avatar')) {
+            $avatar = $request->file('avatar');
+            $filename = Str::random(6) . '.' . $avatar->getClientOriginalExtension();
+
+            // upload ke folder
+            Storage::disk('public')->put($filename, file_get_contents($avatar));
+            $validated['avatar'] = $filename;
+        } else {
+            $validated['avatar'] = $akun->avatar;
+        }
+
+        // simpan data ke database
+        $akun->update([
+            'username' => $validated['username'],
+            'avatar' => $validated['avatar'],
+            'nama_depan' => $validated['nama_depan'],
+            'nama_belakang' => $validated['nama_belakang'],
+            'nik' => $validated['nik'],
+            'alamat' => $validated['alamat'],
+            'rt' => $validated['rt'],
+            'rw' => $validated['rw'],
+            'kelurahan' => $validated['kelurahan'],
+            'kecamatan' => $validated['kecamatan'],
+            'is_admin' => $validated['is_admin'],
+        ]);
+
+        // check password
+        if ($request->has('password')) {
+            $akun->update([
+                'password' => bcrypt($request->password),
+            ]);
+        }
+
+        // redirect ke halaman show
+        return redirect()->route('akun.show', $akun->id);
     }
 
     public function destroy($id)
     {
-        //
+        $akun = User::findOrFail($id);
+        $akun->delete();
+
+        return redirect()->route('akun.index')->with('success', 'Data berhasil dihapus');
     }
 }
